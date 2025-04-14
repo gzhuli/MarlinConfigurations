49b3afa5957bdf17e7c68a1fe78728a433459586
Scott Lahteine <thinkyhead@users.noreply.github.com>
2025-04-14 16:58:45 -0500

    🔧 Increase PID_FUNCTIONAL_RANGE
    MarlinFirmware/Marlin#27740
    
    Co-Authored-By: feldi12 <25982433+feldi12@users.noreply.github.com>

--- config/examples/Creality/Ender-5 S1/Configuration.h
+++ config/examples/Creality/Ender-5 S1/Configuration.h
@@ -921,7 +921,7 @@
   //#define PID_OPENLOOP          // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS      // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
   // The current temperature and target temperature differences start to control the temperature of the PID algorithm. Rock_20220916
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 20 // If the temperature difference between the target temperature and the actual temperature
                                   // is more than PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
 
   #define PID_EDIT_MENU           // Add PID editing to the "Advanced Settings" menu. (~700 bytes of flash)
